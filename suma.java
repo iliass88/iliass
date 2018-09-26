89d5605267d89f10e29bb62f262f97f2791fdb90
iliass ili <iliassili88@gmail.com>
2018-09-26 15:30:46 +0200

    idioma canviat a catala

--- suma.java
+++ suma.java
@@ -2,9 +2,9 @@
 int numero1 = 0;
 int numero2 = 0;
 
-System.out.println("Introduce el primer número:");
+System.out.println("Introdueix el primer nombre:");
 numero1 = reader.nextInt();
 
-System.out.println("Introduce el primer número:");
+System.out.println("Introduieix el sego nombre");
 numero2 = reader.nextInt();
 

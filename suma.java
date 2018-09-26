a0b4a7ff6503e429d419ab3c07ecf540831a5069
iliass88 <43611536+iliass88@users.noreply.github.com>
2018-09-26 16:28:49 +0200

    Update suma.java

--- suma.java
+++ suma.java
@@ -1,9 +1,13 @@
-
-int numero1 = 0;
-int numero2 = 0;
-
-System.out.println("Enter a first number");
-numero1 = reader.nextInt();
-
-System.out.println("Enter a second number");
-
+int num1=30;
+        int num2=15;
+ 
+        if (num1>=num2){
+           
+            if(num1==num2){
+                System.out.println("Los numeros "+num1+" y "+num2+" son iguales");
+            }else{
+                System.out.println("El número "+num1+" es mayor que el número "+num2);
+            }
+        }else{
+            System.out.println("El número "+num2+" es mayor que el número "+num1);
+        }
